6c88cc043095d25c1aba4cde8d3931a641a67534
Jonathan Toews <jon@MacBook-Pro.hitronhub.home>
2017-12-12 00:07:56 -0600

    set header on each request

--- client/src/api.js
+++ client/src/api.js
@@ -2,14 +2,17 @@ import axios from 'axios';
 import auth from 'utils/auth';
 
 // set default authentication header
-axios.defaults.headers.common['Authorization'] = `Bearer ${auth.getToken()}`;
+axios.defaults.headers.common['Authorization'] = `Bearer ${auth.getToken()}`
+
 
 
 export function getLiftTypes() {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${auth.getToken()}`  
   return axios.get('/api/lift-types')
 }
 
 export function getLifts(params) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${auth.getToken()}`
   console.log(params)
   if (params)
     return axios.get('/api/lifts', {params})
@@ -18,13 +21,16 @@ export function getLifts(params) {
 }
 
 export function addLift(data) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${auth.getToken()}`
   return axios.post('/api/lifts', { data })
 }
 
 export function editLift(data) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${auth.getToken()}`
   return axios.put('/api/lifts', { data })
 }
 
 export function deleteLift(id) {
+  axios.defaults.headers.common['Authorization'] = `Bearer ${auth.getToken()}`
   return axios.delete('/api/lifts', { params: { _id: id } })
 }
