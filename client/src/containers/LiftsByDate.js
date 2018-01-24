400e3b52e454f5c53c71c4603f31a7af5c151fdb
Jonathan Toews <jon@JTs-MacBook-Pro.local>
2018-01-23 22:36:35 -0600

    fetch lifts from server on load

--- client/src/containers/LiftsByDate.js
+++ client/src/containers/LiftsByDate.js
@@ -1,27 +1,54 @@
+import React, { Component } from "react"
 import { connect } from "react-redux"
 import {
   editLift,
   addLift,
   deleteLift,
-  setSelectedLift
+  setSelectedLift,
+  fetchLifts
 } from "../actions/index"
-import { getLiftsByDate } from 'reducers'
+import { withRouter } from "react-router-dom"
+import { getLiftsByDate } from "reducers"
 import LiftListByDate from "components/LiftListByDate"
+import FetchError from "components/FetchError"
 
+class LiftsByDate extends Component {
+  render() {
+    return <LiftListByDate {...this.props} />
+  }
 
-const mapStateToProps = state => ({
-  liftsByDate: getLiftsByDate(state),
-  selectedLift: state.selectedLift
-})
+  componentDidMount() {
+    this.fetchData()
+  }
+
+  componentDidUpdate(prevProps) {
+  }
+
+  fetchData() {
+    const { fetchLifts } = this.props
+    fetchLifts()
+  }
+}
+
+const mapStateToProps = (state) => {
+  console.log('lifts by date', getLiftsByDate(state))
+  return {
+    liftsByDate: getLiftsByDate(state),
+    selectedLift: state.selectedLift,
+  }
+}
 
 const mapDispatchToProps = dispatch => ({
   onLiftSubmit: lift => dispatch(addLift(lift)),
   onLiftDelete: liftId => dispatch(deleteLift(liftId)),
   onLiftEdit: (liftId, lift) => dispatch(editLift(liftId, lift)),
   onLiftSelect: liftId => dispatch(setSelectedLift(liftId)),
-  onLiftDeselect: liftId => dispatch(setSelectedLift(null))
+  onLiftDeselect: liftId => dispatch(setSelectedLift(null)),
+  fetchLifts: () => dispatch(fetchLifts())
 })
 
-const LiftsByDate = connect(mapStateToProps, mapDispatchToProps)(LiftListByDate)
+LiftsByDate = withRouter(
+  connect(mapStateToProps, mapDispatchToProps)(LiftsByDate)
+)
 
 export default LiftsByDate
