28f18eea627107491776fdb915fe308ec589283d
Jonathan Toews <jon@JTs-MacBook-Pro.local>
2018-01-23 21:39:25 -0600

    replace custom middleware with npm packages

--- client/src/configureStore.js
+++ client/src/configureStore.js
@@ -1,51 +1,33 @@
-import { createStore } from "redux"
+import { createStore, applyMiddleware, compose } from "redux"
 import reducer from "reducers"
-import sampleData from "./sampleData"
 
-import { loadState, saveState } from "./localStorage"
+import logger from 'redux-logger'
+import promise from 'redux-promise'
+import thunk from 'redux-thunk'
 
-// monkeypatching dispatch examples
-const addLoggingToDispatch = (store) => {
-  const next = store.dispatch
-  return (action) => {
-    console.group(action.type)
-    console.log('prev state:', store.getState())
-    console.log('action:', action)
-    const result = next(action)
-    console.log('new state:', store.getState())
-    console.groupEnd()
-    return result
-  }
-}
-
-const addPromiseToDispatch = (store) => {
-  const next = store.dispatch
-  return (action) => {
-    if (typeof action.then === 'function') {
-      return action.then(next)
-    }
-    return next(action)
-  }
+const applyMiddlewareToStore = (store, middlewares) => {
+  middlewares.slice().reverse().forEach(middleware => {
+    store.dispatch = middleware(store)(store.dispatch)
+  })
 }
 
 const configureStore = () => {
-  const persistedState = loadState()
-  // const persistedState = sampleData
+  const middlewares = [thunk, logger]
 
   const store = createStore(
     reducer,
-    persistedState,
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    compose(
+      applyMiddleware(...middlewares),
+      window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+    )
   )
-  
-  store.dispatch = addLoggingToDispatch(store)
-  store.dispatch = addPromiseToDispatch(store)
 
   store.subscribe(() => {
     saveState({
       lifts: store.getState().lifts
     })
   })
+
   return store
 }
 
